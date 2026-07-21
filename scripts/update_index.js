6968e68886c0c05f9b610222609e679f2ef4266d
manuelgarciacr <29376434+manuelgarciacr@users.noreply.github.com>
2026-07-21 10:34:20 +0200

    En la rama main  Tu rama está actualizada con 'origin/main'.
     Cambios a ser confirmados:
    	modificados:     .github/workflows/update_index.yml
    	nuevos archivos: .github/workflows/update_index.yml.new
    	modificados:     scripts/update_index.js
    	nuevos archivos: scripts/update_index.js.new

--- scripts/update_index.js
+++ scripts/update_index.js
@@ -5,203 +5,152 @@
 //require("dotenv").config();
 
 const https = require("https");
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const fs = require("fs");
 const core = require("@actions/core");
 
 const $GITHUB_TOKEN = process.env.PAT_GITHUB_TOKEN;
 const $USER = process.env.GITHUB_REPOSITORY_OWNER;
-const connectApiHost = "api.github.com"
+const connectApiHost = "api.github.com";
 //const connectApiPath = `/users/${$USER}/repos`
 const connectApiPath = `/user/repos?per_page=100`;
 const headers = {
     "User-Agent": "request",
-    "Authorization": `token ${$GITHUB_TOKEN}`
-}
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const data = []
+    Authorization: `token ${$GITHUB_TOKEN}`,
+};
+const data = [];
 
 let options = {
     hostname: connectApiHost,
     path: connectApiPath,
     port: 443,
     method: "GET",
-    headers
+    headers,
 };
 
-console.log("USER: ", $USER);
-console.log("OPTIONS: ", options);
+console.log(headers.Authorization.length);
+https
+    .request(options, res => {
+        let strData = "";
 
-async function main() {
-    try {
-console.log("***************************ENTRO MAIN")
-        const reposData = await getAllRepos();
-console.log("***************************SALGO MAIN", reposData.length)
-        const repos = [];
+        res.on("error", err => {
+            core.setFailed(
+                `Request for repos failed: ${JSON.stringify(err, null, 4)}`,
+            );
+        });
 
-        for (const ele  of reposData) {
-            if (ele.owner.login !== "$USER")
-                continue;
-            const repo = {
-                name: ele.name,
-                description: (ele.description || "").trim(),
-                repository: ele.html_url,
-                webpage: ele.has_pages
-                    ? `https://manuelgarciacr.github.io/${ele.name}`
-                    : "",
-                created: ele.created_at.substring(0, 21).replace("-", "/"),
-                pushed: ele.pushed_at.substring(0, 21).replace("-", "/"),
-                topics: ele.topics || [],
-                private: ele.visibility === "private",
-            };
-            const languagesPromise = getLanguages(ele.languages_url);
-            const subtopicsPromise = getSubtopics(ele.name);
+        res.on("data", chunk => {
+            strData += chunk.toString();
+        });
 
-            await languagesPromise
-                .then(res => {
-                    const entries = Object.entries(res).sort((a, b) => b[1] - a[1]);
-                    const total = entries.reduce(
-                        (prev, curr) => (prev += curr[1]),
-                        0,
-                    );
+        res.on("end", async () => {
+            if (res.statusCode !== 200) {
+                core.setFailed(
+                    `Request for repos failed: ${JSON.stringify(res.statusMessage, null, 4)}`,
+                );
+                return;
+            }
+            const data = await getRepos($USER, strData);
 
-                    repo.languages = {};
-                    entries.forEach(
-                        entry =>
-                            (repo.languages[entry[0]] =
-                                Math.round((entry[1] * 1000) / total) / 10),
-                    );
-                })
-                .catch(err =>
-                    core.setFailed(
-                        `Request for languages failed: ${JSON.stringify(
-                            err,
-                            null,
-                            4,
-                        )}`,
-                    ),
+            if (process.exitCode ?? 0) return;
+
+            console.log("RES", res.headers);
+
+            createFile(data);
+            sortTopics("topics");
+            sortTopics("subtopics");
+
+            core.info(fs.readdirSync("./src/assets").join("\n"));
+        });
+    })
+    .end();
+
+const getRepos = async (user, strData) => {
+    const body = JSON.parse(strData);
+
+    for (const ele of body) {
+        const repo = {
+            name: ele.name,
+            description: (ele.description || "").trim(),
+            repository: ele.html_url,
+            webpage: ele.has_pages
+                ? `https://manuelgarciacr.github.io/${ele.name}`
+                : "",
+            created: ele.created_at.substring(0, 21).replace("-", "/"),
+            pushed: ele.pushed_at.substring(0, 21).replace("-", "/"),
+            topics: ele.topics || [],
+            private: ele.visibility === "private",
+        };
+        const languagesPromise = getLanguages(ele.languages_url);
+        const subtopicsPromise = getSubtopics(ele.name);
+
+        await languagesPromise
+            .then(res => {
+                const entries = Object.entries(res).sort((a, b) => b[1] - a[1]);
+                const total = entries.reduce(
+                    (prev, curr) => (prev += curr[1]),
+                    0,
                 );
 
-            if (process.exitCode ?? 0) return;
+                repo.languages = {};
 
-            await subtopicsPromise
-                .then(res => {
-                    const variables = JSON.parse(res).variables;
-                    const subtopicsVar = variables?.find(
-                        v => v.name === "SUBTOPICS",
-                    );
-                    const subtopics = JSON.parse(subtopicsVar?.value ?? "[]");
-                    const showIdx = subtopics.indexOf("show-private");
-                    repo.subtopics = subtopics.sort((a, b) => a - b);
+                entries.forEach(
+                    entry =>
+                        (repo.languages[entry[0]] =
+                            Math.round((entry[1] * 1000) / total) / 10),
+                );
+            })
+            .catch(err =>
+                core.setFailed(
+                    `Request for languages failed: ${JSON.stringify(
+                        err,
+                        null,
+                        4,
+                    )}`,
+                ),
+            );
 
-                    if (!repo.private || showIdx >= 0) {
-                        repo.show = true;
-                    } else {
-                        repo.show = false;
-                    }
-                    //core.info("TYPE: " + typeof res);
-                    //core.info("SUBTOPICS: " + JSON.stringify(JSON.parse(res), null, 4));
-                })
-                .catch(err => {
-                    core.error(err);
-                    core.setFailed(
-                        `Request for subtopics failed: ${JSON.stringify(
-                            err,
-                            null,
-                            4,
-                        )}`,
-                    );
-                });
+        if (process.exitCode ?? 0) return;
 
-            if (process.exitCode ?? 0) return;
+        await subtopicsPromise
+            .then(res => {
+                const variables = JSON.parse(res).variables;
+                const subtopicsVar = variables?.find(
+                    v => v.name === "SUBTOPICS",
+                );
+                const subtopics = JSON.parse(subtopicsVar?.value ?? "[]");
+                const showIdx = subtopics.indexOf("show-private");
+                repo.subtopics = subtopics.sort((a, b) => a - b);
 
-            if (ele.has_pages) {
-                repo.page = `https://${$USER}.github.io/${ele.name}`;
-            }
+                if (!repo.private || showIdx >= 0) {
+                    repo.show = true;
+                } else {
+                    repo.show = false;
+                }
+                core.info(typeof res);
+                core.info(JSON.stringify(JSON.parse(res), null, 4));
+            })
+            .catch(err => {
+                core.error(err);
+                core.setFailed(
+                    `Request for subtopics failed: ${JSON.stringify(
+                        err,
+                        null,
+                        4,
+                    )}`,
+                );
+            });
 
-            repos.push(repo);
+        if (process.exitCode ?? 0) return;
+
+        if (ele.has_pages) {
+            // repo.page = "https://$USER.github.io/" + ele.name;
+            repo.page = `https://${user}.github.io/${ele.name}`;
         }
 
-        createFile(repos);
-        sortTopics("topics");
-        sortTopics("subtopics");
-
-        console.log(repos);
-        if (reposData.length)
-            console.log(reposData[reposData.length - 1]);
-        console.log("NUM OF REPOS:", repos.length);
-    } catch (err) {
-        core.setFailed(`Request for repos failed: ${err.message}`);
+        data.push(repo);
     }
-}
-
-async function getAllRepos() {
-    let url = "https://api.github.com/user/repos?per_page=20";
-    const repos = [];
-
-    while (url) {
-        const result = await request(url);
-
-        repos.push(...result.repos);
-
-        url = getNext(result.link);
-    }
-
-    return repos;
-}
-
-function request(url) {
-    return new Promise((resolve, reject) => {
-        const req = https.request(
-            url,
-            {
-                headers: {
-                    "User-Agent": "node",
-                    //Accept: "application/vnd.github+json",
-                    Authorization: `token ${$GITHUB_TOKEN}`,
-                },
-            },
-            res => {
-                let body = "";
-
-                res.on("data", chunk => (body += chunk));
-
-                res.on("end", () => {
-                    if (res.statusCode !== 200) {
-                        console.log(
-                            "*****************ERROR !== 200",
-                            $GITHUB_TOKEN.length,
-                            req.getHeaders
-                        );
-                        return reject(
-                            new Error(`HTTP ${res.statusCode}: ${body} ${req}`),
-                        );
-                    }
-                    console.log("******************200");
-                    resolve({
-                        repos: JSON.parse(body),
-                        link: res.headers.link,
-                    });
-                });
-            },
-        );
-
-        req.on("error", reject);
-        req.end();
-    });
-}
-
-function getNext(link) {
-    if (!link) return null;
-
-    for (const item of link.split(",")) {
-        const parts = item.trim().split(";");
-
-        if (parts[1].includes('rel="next"')) return parts[0].slice(1, -1);
-    }
-
-    return null;
-}
+    return data;
+};
 
 const getLanguages = url => {
     return new Promise((response, reject) =>
@@ -225,7 +174,7 @@ const getLanguages = url => {
 
 const getSubtopics = repo => {
     const url = `https://api.github.com/repos/${$USER}/${repo}/actions/variables`;
-    //core.info(`URL: ${url}`)
+    core.info(`URL: ${url}`);
 
     return new Promise((response, reject) =>
         https
@@ -277,259 +226,3 @@ const sortTopics = name => {
         core.setFailed(`Sort ${name}.json file failed`);
     }
 };
-
-main();
-
-return
-/*
-https.request(options, res => {
-    let strData = "";
-
-    res.on("error", err => {
-        core.setFailed(`Request for repos failed: ${JSON.stringify(err, null, 4)}`);
-    });
-
-    res.on("data", chunk => {
-        strData += chunk.toString();
-    });
-
-    res.on("end", async () => {
-        if (res.statusCode !== 200) {
-            core.setFailed(
-                `Request for repos failed: ${JSON.stringify(res.statusMessage, null, 4)}`
-            );
-            return
-        }
-        const data = await getRepos($USER, strData);
-
-        if (process.exitCode ?? 0) return;
-
-        console.log("RES:", res.headers);
-        //console.log("REPOS: ", data);
-        createFile(data);
-        sortTopics("topics");
-        sortTopics("subtopics");
-
-        core.info(fs.readdirSync("./src/assets").join("\n"));
-
-        const reposData = await getAllRepos();
-        const repos = [];
-
-        for (const ele of reposData) {
-            const repo = {
-                name: ele.name,
-                description: (ele.description || "").trim(),
-                repository: ele.html_url,
-                webpage: ele.has_pages
-                    ? `https://manuelgarciacr.github.io/${ele.name}`
-                    : "",
-                created: ele.created_at.substring(0, 21).replace("-", "/"),
-                pushed: ele.pushed_at.substring(0, 21).replace("-", "/"),
-                topics: ele.topics || [],
-                private: ele.visibility === "private",
-            };
-            const languagesPromise = getLanguages(ele.languages_url);
-            const subtopicsPromise = getSubtopics(ele.name);
-
-            await languagesPromise
-                .then(res => {
-                    const entries = Object.entries(res).sort(
-                        (a, b) => b[1] - a[1],
-                    );
-                    const total = entries.reduce(
-                        (prev, curr) => (prev += curr[1]),
-                        0,
-                    );
-
-                    repo.languages = {};
-                    entries.forEach(
-                        entry =>
-                            (repo.languages[entry[0]] =
-                                Math.round((entry[1] * 1000) / total) / 10),
-                    );
-                })
-                .catch(err =>
-                    core.setFailed(
-                        `Request for languages failed: ${JSON.stringify(
-                            err,
-                            null,
-                            4,
-                        )}`,
-                    ),
-                );
-
-            if (process.exitCode ?? 0) return;
-
-            await subtopicsPromise
-                .then(res => {
-                    const variables = JSON.parse(res).variables;
-                    const subtopicsVar = variables?.find(
-                        v => v.name === "SUBTOPICS",
-                    );
-                    const subtopics = JSON.parse(subtopicsVar?.value ?? "[]");
-                    const showIdx = subtopics.indexOf("show-private");
-                    repo.subtopics = subtopics.sort((a, b) => a - b);
-
-                    if (!repo.private || showIdx >= 0) {
-                        repo.show = true;
-                    } else {
-                        repo.show = false;
-                    }
-                    //core.info("TYPE: " + typeof res);
-                    //core.info("SUBTOPICS:
-                });
-
-            if (process.exitCode ?? 0) return;
-
-            if (ele.has_pages) {
-                repo.page = `https://${$USER}.github.io/${ele.name}`;
-            }
-
-            repos.push(repo);
-        }
-        console.log(repos);
-        console.log("NUM OF REPOS:", repos.length);
-    })" + JSON.stringify(JSON.parse(res), null, 4));
-                })
-                .catch(err => {
-                    core.error(err);
-                    core.setFailed(
-                        `Request for subtopics failed: ${JSON.stringify(
-                            err,
-                            null,
-                            4,
-                        )}`,
-                    );
-                });
-
-            if (process.exitCode ?? 0) return;
-
-                });
-
-            if (process.exitCode ?? 0) return;
-
-            if (ele.has_pages) {
-                repo.page = `https://${$USER}.github.io/${ele.name}`;
-            }
-
-            repos.push(repo);
-        }
-        console.log(repos);
-        console.log("NUM OF REPOS:", repos.length);
-    })
-            if (ele.has_pages) {
-                repo.page = `https://${$USER}.github.io/${ele.name}`;
-            }
-
-            repos.push(repo);
-        }
-        console.log(repos);
-        console.log("NUM OF REPOS:", repos.length);
-    })
-}).end();
-
-const getRepos = async (user, strData) => {
-    const body = JSON.parse(strData);
-
-    //console.log("REPOS DATA: ", body);
-    for (const ele  of body) {
-        const repo = {
-            name: ele.name,
-            description: (ele.description || "").trim(),
-            repository: ele.html_url,
-            webpage: ele.has_pages
-                ? `https://manuelgarciacr.github.io/${ele.name}`
-                : "",
-            created: ele.created_at.substring(0, 21).replace("-", "/"),
-            pushed: ele.pushed_at.substring(0, 21).replace("-", "/"),
-            topics: ele.topics || [],
-            private: ele.visibility === "private"
-        };
-        const languagesPromise = getLanguages(ele.languages_url);
-        const subtopicsPromise = getSubtopics(ele.name);
-
-        await languagesPromise
-            .then(res => {
-                const entries = Object.entries(res).sort((a, b) => b[1] - a[1]);
-                const total = entries.reduce(
-                    (prev, curr) => (prev += curr[1]),
-                    0
-                );
-
-                repo.languages = {};
-
-                entries.forEach(
-                    entry =>
-                        (repo.languages[entry[0]] =
-                            Math.round((entry[1] * 1000) / total) / 10)
-                );
-            })
-            .catch(err =>
-                core.setFailed(
-                    `Request for languages failed: ${JSON.stringify(
-                        err,
-                        null,
-                        4
-                    )}`
-                )
-            );
-
-        if (process.exitCode ?? 0) return;
-
-        await subtopicsPromise
-            .then(res => {
-                const variables = JSON.parse(res).variables
-                const subtopicsVar =  variables?.find(v => v.name === "SUBTOPICS");
-                const subtopics =  JSON.parse(subtopicsVar?.value ?? "[]");
-                const showIdx = subtopics.indexOf("show-private");
-                repo.subtopics = subtopics.sort((a, b) => a - b);
-
-                if (!repo.private || showIdx >= 0) {
-                    repo.show = true;
-                } else {
-                    repo.show = false
-                }
-                //core.info("TYPE: " + typeof res);
-                //core.info("SUBTOPICS: " + JSON.stringify(JSON.parse(res), null, 4));
-            })
-            .catch(err => {
-                core.error(err);
-                core.setFailed(
-                    `Request for subtopics failed: ${JSON.stringify(
-                        err,
-                        null,
-                        4
-                    )}`
-                )
-            });
-
-        if (process.exitCode ?? 0) return;
-
-        if (ele.has_pages) {
-            // repo.page = "https://$USER.github.io/" + ele.name;
-            repo.page = `https://${ user }.github.io/${ ele.name }`
-        }
-
-        data.push(repo);
-    }
-    return data
-}
-
-
-
-const createFile = (data) => {
-    //process.stdout.write(JSON.stringify(data, null, 4));
-    try {
-        fs.writeFileSync(
-            "./src/assets/data.json",
-            JSON.stringify(data, null, 4),
-        );
-        core.info("Data written to file successfully.")
-    } catch(err) {
-        core.setFailed(
-            `Write data.json file failed: ${JSON.stringify(err, null, 4)}`
-        );
-    }
-}
-
-*/
